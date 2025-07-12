31bf26409ed3a05a3bb8b342c14f8635e04acee5
Devvora <devvora08@gmail.com>
2025-07-12 11:10:22 +0530

    Integrate innegest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client"
 
-function page() {
+import { Button } from '@/components/ui/button'
+import { useTRPC } from '@/trpc/client'
+import { useMutation } from '@tanstack/react-query'
+import React from 'react'
+import { toast } from 'sonner'
+
+function Page() {
+
+  const trpc = useTRPC()
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background job started")
+    },
+  }))
+
   return (
-    <div>
-      
+    <div className='p-4 max-w-7xl mx-auto'>
+      <Button onClick={() => invoke.mutate({text: "Dev Vora"})}>
+        Invoke Job
+      </Button>
     </div>
   )
 }
 
-export default page
+export default Page
