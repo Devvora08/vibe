fa4203231b5cc64591445731df9adacfb3a62412
Devvora <devvora08@gmail.com>
2025-07-13 10:41:32 +0530

    inngest ai jobs tested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { useTRPC } from '@/trpc/client'
 import { useMutation } from '@tanstack/react-query'
-import React from 'react'
+import React, { useState } from 'react'
 import { toast } from 'sonner'
 
 function Page() {
 
   const trpc = useTRPC()
+  const [value, setValue] = useState('')
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
       toast.success("Background job started")
@@ -17,7 +19,8 @@ function Page() {
 
   return (
     <div className='p-4 max-w-7xl mx-auto'>
-      <Button onClick={() => invoke.mutate({text: "Dev Vora"})}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)}/>
+      <Button onClick={() => invoke.mutate({input: value})}>
         Invoke Job
       </Button>
     </div>
